4c56ff3804eb027dd1da582ce0d7a3b9c0902571
Patrick Herbers <patrick.herbers@rub.de>
2021-10-02 12:23:19 +0200

    Added Truck Anim

--- Assets/Scripts/Truck.cs
+++ Assets/Scripts/Truck.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Truck : MonoBehaviour
-{
+public class Truck : MonoBehaviour {
+    public Animator truckAnimator;
+
+    private float timer = 2.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        truckAnimator.Play("TruckEnter");
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        timer -= Time.deltaTime;
+        if(timer < 0)
+            truckAnimator.Play("TruckExit");
     }
 }
