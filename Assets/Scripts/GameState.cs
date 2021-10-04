128f70bd49ff31649e1ede6707bc7e862303db55
Nils Fo <nils.foerster@yahoo.de>
2021-10-04 18:39:33 +0200

    main scene test

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -27,6 +27,8 @@ public class GameState : MonoBehaviour
     public SpawnCargo cargoSpawner;
     public ToolConveyor toolConveyor;
     public StartButtonTruck startBT;
+    
+    public bool tutorialHasTapeDroppedAtLeastOnce = false;
 
     public bool tutorialRunning = false;
     public bool forceNextDriverQuote = false;
@@ -74,6 +76,7 @@ public class GameState : MonoBehaviour
 
             startBT.myText.enabled = false;
         }
+        tutorialHasTapeDroppedAtLeastOnce = false;
 
         if (GameState.shift == 1)
         {
